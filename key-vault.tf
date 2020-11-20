7d0f54786fe2df3dd624b7e99d572a0a62727f28
Massimo Cannavo <massimo5cannavo@gmail.com>
2020-11-19 21:40:42 -0700

    Added key vault resource

---
+++ key-vault.tf
@@ -0,0 +1,36 @@
+resource azurerm_key_vault key_vault {
+  name = format("%s-%s-%03d",
+    substr(
+      module.naming.key_vault.name, 0,
+      module.naming.key_vault.max_length - 6
+    ),
+    substr(local.environment, 0, 3),
+    var.info.sequence
+  )
+
+  resource_group_name = var.resource_group
+  location            = var.region
+
+  sku_name  = var.sku
+  tenant_id = data.azurerm_client_config.azure.tenant_id
+
+  enabled_for_deployment          = var.enabled_for_deployment
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+
+  enable_rbac_authorization = var.enable_rbac
+  purge_protection_enabled  = var.purge_protection_enabled
+
+  soft_delete_enabled        = var.soft_delete_enabled
+  soft_delete_retention_days = var.soft_delete_retention
+
+  network_acls {
+    default_action = var.default_action
+    bypass         = var.bypass
+
+    ip_rules                   = local.ip_whitelist
+    virtual_network_subnet_ids = local.subnet_whitelist
+  }
+
+  tags = local.tags
+}
---
+++ main.tf
@@ -0,0 +1,47 @@
+# Environment variables should be used for authentication.
+#
+# ARM_SUBSCRIPTION_ID
+# ARM_CLIENT_ID
+# ARM_CLIENT_SECRET
+# ARM_TENANT_ID
+#
+# Reference the Azure Provider documentation for more information.
+#
+# https://www.terraform.io/docs/providers/azurerm/index.html
+
+provider azurerm {
+  features {}
+}
+
+terraform {
+  required_version = "~> 0.13.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.35.0"
+    }
+  }
+}
+
+locals {
+  project     = lower(var.info.project)
+  environment = lower(var.info.environment)
+
+  tags = merge(
+    {
+      for key, value in var.tags : lower(key) => lower(value)
+    },
+    {
+      project     = local.project
+      environment = local.environment
+    }
+  )
+}
+
+module naming {
+  source  = "Azure/naming/azurerm"
+  version = "0.1.0"
+
+  suffix = [local.project]
+}
