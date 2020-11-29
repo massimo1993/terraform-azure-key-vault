2a4eafd09ac2521a7c8154a7507f2c9f33f8156e
Massimo Cannavo <massimo5cannavo@gmail.com>
2020-11-28 17:23:40 -0700

    Upgraded azurerm provider to version 2.35.0

--- examples/import-cert/main.tf
+++ examples/import-cert/main.tf
@@ -11,7 +11,7 @@
 
 module resource_group {
   source  = "massimo1993/resource-group/azure"
-  version = "0.0.1"
+  version = "0.0.2"
 
   info = var.info
   tags = var.tags
@@ -21,7 +21,7 @@ module resource_group {
 
 module key_vault {
   source  = "massimo1993/key-vault/azure"
-  version = "0.0.1"
+  version = "0.0.2"
 
   info = var.info
   tags = var.tags
